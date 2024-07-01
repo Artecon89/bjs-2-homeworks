0de0d5f6bf97c81213e1fd2642b11e6cdb3e0deb
Artecon <info@alexey01.ru>
2024-07-01 21:47:09 +0300

    Решение Задач

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [Math.pow(b,2)-4*a*c];
-  
-  return arr;
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  }
+if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+    return arr;
 }
-
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+    return arr;
+  }
+  
+}
+console.log(solveEquation(-1, 3, 4))
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = percent / 100 / 12;
+  let payment = amount - contribution;
+  let monthlyPayment = payment * (p + (p / (((1 + p) ** countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return +totalAmount.toFixed(2);
 }
+console.log(calculateTotalMortgage(10, 0, 50000, 12))
